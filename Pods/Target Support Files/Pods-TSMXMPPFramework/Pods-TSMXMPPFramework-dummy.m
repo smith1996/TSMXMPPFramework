8509a7d0bb733846056388fc3057057bfc2b7a84
Smith Huamani Hilario <>
2018-04-13 16:52:04 -0500

    first commit xmpp

---
+++ Pods/Target Support Files/Pods-TSMXMPPFramework/Pods-TSMXMPPFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TSMXMPPFramework : NSObject
+@end
+@implementation PodsDummy_Pods_TSMXMPPFramework
+@end
---
+++ TSMXMPPFramework/TSMXMPPFramework.h
@@ -0,0 +1,19 @@
+//
+//  TSMXMPPFramework.h
+//  TSMXMPPFramework
+//
+//  Created by Smith Huamani Hilario on 4/13/18.
+//  Copyright © 2018 demos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSMXMPPFramework.
+FOUNDATION_EXPORT double TSMXMPPFrameworkVersionNumber;
+
+//! Project version string for TSMXMPPFramework.
+FOUNDATION_EXPORT const unsigned char TSMXMPPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSMXMPPFramework/PublicHeader.h>
+
+
